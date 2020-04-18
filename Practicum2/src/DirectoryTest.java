3570fe536d94c47d7a6bd19b543190e70cef0a08
Art Willems <darthmundus038@gmail.com>
2020-04-18 14:36:56 +0200

    update directoryTest

--- Practicum2/src/DirectoryTest.java
+++ Practicum2/src/DirectoryTest.java
@@ -1,4 +1,3 @@
-package Practicum2.src; 
 /**
  *  A class with test function for public methods of the class Directory
  */
@@ -132,6 +131,7 @@ public class DirectoryTest {
 	@Test
 	public void testForGetList(){
 		// initialise some files and directories and add them to an empty Arraylist
+		// the exact same files are added in the right way, so the two lists are exact copies of eachother
 		OuterDir = new Directory("OuterDir", null, true); 
 		InnerDir = new Directory("InnerDir",OuterDir, false); 
 		File SomeFile = new File(InnerDir, "ogp", 20, false, "java"); 
@@ -174,7 +174,6 @@ public class DirectoryTest {
 		
 	}
 	
-	//normally, following test should be applicable for FileSystem, but only for Directories
 	@Test
 	public void testListEmpty_falseCase() {
 		Directory NotNullDirectory = new Directory("justanotherdirectory"); 
@@ -183,16 +182,32 @@ public class DirectoryTest {
 		assertFalse(NotNullDirectory.isListEmpty()); 
 	}
 	@Test
-	public void testDirectoryContainsItself_falseCase() {
-		assertFalse(OuterDir.getList().contains(OuterDir)); 
+	public void testIsValidList_legalCase() {
+		assertTrue(OuterDir.isValidList()); 
+	}
+	
+	@Test 
+	public void testIsvalidList_IllegalCase() {
+		OuterDir = new Directory("OuterDir", OuterDir, true); 
+		assertFalse(OuterDir.isValidList()); 
+	}
+	@Test
+	public void testIsValidFileSystem_LegalCase() {
+		FileSystem valid = new FileSystem("valid", DirWritable, true); 
+		assertTrue(OuterDir.isValidFileSystem(valid)); 
 	}
 	
 	@Test
-	public void testDirectoryContainsItself_truecase() {
-		OuterDir = new Directory("OuterDir", OuterDir, true); 
-		assertTrue(OuterDir.getList().contains(OuterDir)); 
+	public void testIsValidFileSystem_IllegalCaseA() { 
+		FileSystem invalid = new FileSystem("invalid", DirWritable, true); 
+		assertFalse(DirWritable.isValidFileSystem(invalid)); 
 	}
 	
+	@Test
+	public void testIsValidFileSystem_IllegalCaseB() {
+		FileSystem invalid = null; 
+		assertFalse(OuterDir.isValidFileSystem(invalid)); 
+	}
 	@Test
 	public void testForRemove_LegalCase() {
 		InnerDir.remove(DirWritable);
@@ -201,14 +216,21 @@ public class DirectoryTest {
 	}
 	
 	@Test (expected = NotInListException.class)
-	public void testForRemove_IllegalCase() {
+	public void testForRemove_IllegalCaseA() {
 		File dummy = new File("dummy"); 
 		InnerDir.remove(dummy);
 		assertNull(InnerDir.getModificationTime()); 
 	}
 	
+	@Test (expected = FileSystemNotWritableException.class)
+	public void testForRemove_IllegalCaseB(){
+		OuterDir = new Directory("OuterDir", null, false); 
+		OuterDir.remove(InnerDir); 
+	}
+	
 	@Test
 	public void testCanHaveAsItem_LegalCase() {
+		// set the directory of InnerDir to OuterDir. Objects within InnerDir will be placed in OuterDir.getList()
 		InnerDir.setDir(OuterDir);
 		assertTrue(OuterDir.hasAsItem(DirWritable)); 
 		
@@ -222,6 +244,7 @@ public class DirectoryTest {
 	
 	@Test
 	public void testgetNbItems() {
+		// set the directory of InnerDir to OuterDir. Objects within InnerDir will be placed in OuterDir.getList()
 		InnerDir.setDir(OuterDir); 
 		assertEquals(3, OuterDir.getNbItems()); 
 	}
@@ -240,12 +263,15 @@ public class DirectoryTest {
 		one = new File(DirWritable, "one", 56, true, "pdf"); 
 		two = new File(DirWritable, "two", 789, true, "java"); 
 		three = new File(DirWritable, "three", 9, true, "txt"); 
+		assertEquals(one, DirWritable.getItemAt(1)); 
 		assertEquals(two, DirWritable.getItemAt(2)); 
+		assertEquals(three, DirWritable.getItemAt(3)); 
 	}
 
 	
 	@Test (expected = IndexOutOfRangeException.class)
 	public void testGetItemAt_IllegalCaseA() {
+		// make three new files for DirWritable
 		File one, two, three; 
 		one = new File(DirWritable, "one", 56, true, "pdf"); 
 		two = new File(DirWritable, "two", 789, true, "java"); 
@@ -268,7 +294,8 @@ public class DirectoryTest {
 	@Test
 	public void testGetIndexOf_legalCaseA() {
 		//testing the accessing of files within directories
-		InnerDir.setDir(OuterDir);; 
+		InnerDir.setDir(OuterDir);
+		//make a new file located in InnerDir. Lexicographically, the new file is placed first in InnerDir.getList()
 		File BeforeDirJustName;
 		BeforeDirJustName = new File(InnerDir, "before", 10, true,"pdf"); 
 		assertEquals(1, OuterDir.getIndexOf(BeforeDirJustName.getName())); 
@@ -277,6 +304,7 @@ public class DirectoryTest {
 	
 	@Test(expected = NotInListException.class)
 	public void testGetIndexOf_IllegalCaseA() {
+		// make a new root file
 		File file = new File("file"); 
 		InnerDir.getIndexOf(file.getName()); 
 		
@@ -291,6 +319,7 @@ public class DirectoryTest {
 	
 	@Test (expected = NotInListException.class)
 	public void testGetIndexOf_IllegalCaseB() {
+		// make a directory that isn't a subdirectory of OuterDir, trying to get index will result in NotInListException
 		Directory DirNotInOuterDir = new Directory("dir", null, true ); 
 		OuterDir.getIndexOf(DirNotInOuterDir.getName()); 
 	}
